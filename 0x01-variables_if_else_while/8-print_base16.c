7a4b7b1559227a4e5c8725210861874c66f787c1
Your Name <you@example.com>
2023-01-28 15:25:31 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - sucess
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'a'; i <= 'f'; i++)
+	{
+	putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
