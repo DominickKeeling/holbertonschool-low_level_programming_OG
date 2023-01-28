c4cbb39d6702098164f95555361d4f312ce6d58d
Your Name <you@example.com>
2023-01-28 11:32:40 -0800

    Alphabet minus e and  q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - write a program that prints the alphabet in lowercase with \n
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch == 'e' || ch == 'q')
+	{
+		continue;
+	}
+	putchar(ch);
+	}
+	return (0);
+}
