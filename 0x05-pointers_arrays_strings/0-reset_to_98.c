cf0dae2f21dc3683233cb33b6617893eb26f9025
Your Name <you@example.com>
2023-02-06 10:43:18 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
 * reset_to_98 - write a function that takes a pointer to an int as parameter
 * and updates the value it pointes to to 98
-* @*n: the variable to check
+* @n: the variable to check
 * Return: something
 */
 void reset_to_98(int *n)
