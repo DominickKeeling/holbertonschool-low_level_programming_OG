b5c28e5bee2d653eea9fe67a52b906987f7a5c13
Your Name <you@example.com>
2023-02-06 21:49:46 -0800

    Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_array - write a function that prints n elements of an array 
+* print_array - write a function that prints n elements of an array
 * of integers and \n
 * @a: int to check
 * @n: int to check
