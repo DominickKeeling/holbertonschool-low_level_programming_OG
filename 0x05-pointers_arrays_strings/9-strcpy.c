d8831d7072e1c9e6268fc106dd66a4a4a7b63351
Your Name <you@example.com>
2023-02-06 22:01:36 -0800

    strcpy.

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,6 +4,8 @@
 * _strcpy - write a function that copys the string pointed to by src
 * including the terminating null byte(\0) to the buffer pointed
 * to by dest
+* @dest: destination
+* @src: source
 * Return: pointer to dest
 */
 char *_strcpy(char *dest, char *src)
