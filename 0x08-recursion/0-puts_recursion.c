a0bf25fc34149a1b66e3b49b9a44017bb545694c
Your Name <you@example.com>
2023-02-20 18:59:03 -0800

    print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _puts_recursion - write a function that prints a string \n
+* @s: character to check
+* Return: 0
+*/
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
