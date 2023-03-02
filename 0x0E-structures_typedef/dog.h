d7ae5704890f744410ea61f46c9b68b5fec46a34
Your Name <you@example.com>
2023-03-02 09:44:15 -0800

    poopy poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* struct dog - define a new type of struct dog with the following
+* @name: pointer to a char *
+* @age: type = float
+* @owner: type = char *
+* Return: 0
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
